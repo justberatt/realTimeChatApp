c8eeae36251bf350dbd8754b5d7d4734a4368a26
justberatt <berat.sakipi@gmail.com>
2024-09-29 23:54:26 +0200

    Deleted the node_modules and reversed the app to connect and work with the database through the CDN again. Added a bunch of HTML added some CSS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getDatabase, ref, push, onValue, remove } from "https://real-time-chat-app-57ee0-default-rtdb.europe-west1.firebasedatabase.app"
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js"
 
 const firebaseConfig = {
+    databaseURL: "https://real-time-chat-app-57ee0-default-rtdb.europe-west1.firebasedatabase.app",
     apiKey: "AIzaSyCJBKNFZ6pvSwuwH1ms0Ca3w8p-P0c97WY",
     authDomain: "real-time-chat-app-57ee0.firebaseapp.com",
     projectId: "real-time-chat-app-57ee0",
@@ -12,6 +14,6 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const database = getDatabase(app);
 const referenceInDB = ref(database, "messages")
-const auth = firebase.auth();
