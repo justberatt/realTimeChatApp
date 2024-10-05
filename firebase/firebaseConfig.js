70ff8b41a628cec575ce0064a2cfe463b3425058
justberatt <berat.sakipi@gmail.com>
2024-10-05 23:49:02 +0200

    Rearranged the folder and the files, deleted the email/password files, so now I am only going to be working with google sign in option. Refactored the code

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js"
+
+const firebaseConfig = {
+    databaseURL: "https://real-time-chat-app-57ee0-default-rtdb.europe-west1.firebasedatabase.app",
+    apiKey: "AIzaSyCJBKNFZ6pvSwuwH1ms0Ca3w8p-P0c97WY",
+    authDomain: "real-time-chat-app-57ee0.firebaseapp.com",
+    projectId: "real-time-chat-app-57ee0",
+    storageBucket: "real-time-chat-app-57ee0.appspot.com",
+    messagingSenderId: "487240951338",
+    appId: "1:487240951338:web:49d0381b54839c929f0553"
+  };
+
+export const app = initializeApp(firebaseConfig);
+export const provider = new GoogleAuthProvider();
+export const auth = getAuth(app);
--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-
+import { app, auth } from "./firebase/firebaseConfig.js";
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js"
 
-const firebaseConfig = {
-    databaseURL: "https://real-time-chat-app-57ee0-default-rtdb.europe-west1.firebasedatabase.app",
-    apiKey: "AIzaSyCJBKNFZ6pvSwuwH1ms0Ca3w8p-P0c97WY",
-    authDomain: "real-time-chat-app-57ee0.firebaseapp.com",
-    projectId: "real-time-chat-app-57ee0",
-    storageBucket: "real-time-chat-app-57ee0.appspot.com",
-    messagingSenderId: "487240951338",
-    appId: "1:487240951338:web:49d0381b54839c929f0553"
-  };
-
-
-const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const referenceInDB = ref(database, "messages");
 
